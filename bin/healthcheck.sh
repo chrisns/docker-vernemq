cafe0e94294d61f6392e2f58d3da3ae1a5bfa22b
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-15 18:48:53 +0100

    maybe be more generous with startup times

--- bin/healthcheck.sh
+++ bin/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #give it 60 seconds to start up before testing
-if [ "$(($(date +%s) - $(date +%s -r /proc/1/cmdline)))" -lt "60" ]; then
+if [ "$(($(date +%s) - $(date +%s -r /proc/1/cmdline)))" -lt "90" ]; then
   exit 0;
 fi
 
